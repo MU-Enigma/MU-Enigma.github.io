4332bcd26a06f55563b97ba14f7376ce18f171d5
Androbuddy <anirudhchimpidi8@gmail.com>
2021-08-09 23:41:23 +0530

    Landing page
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header />
+  <div class="flex-grow flex flex-col-reverse lg:flex-row lg:justify-between justify-center items-center">
+    <div class="mx-16 flex flex-col gap-2 lg:gap-5">
+      <h1 class="lg:text-4xl text-2xl font-bold">Enigma</h1>
+      <h2 class="lg:text-4xl text-xl"><span class="text-accent">Computer Science</span> Club</h2>
+      <h3 class="lg:text-2xl text-lg font-thin">In the End, it all comes down to 1’s and 0’s</h3>
+    </div>
+    <img src="./assets/landing_hero.svg" class="lg:w-auto w-0" />
+  </div>
+  <Footer />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&display=swap');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+body {
+  background: url("./assets/landing_bin.svg") #1F2937;
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Mono', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  color: #FFF;
 }
 </style>
---
+++ src/components/Social.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-4">
+    <a href="https://github.com/mec-enigma"><img src="./assets/github.svg" alt=""></a>
+    <a href="" class="hidden"><img src="./assets/linkedlin.svg" alt=""></a>
+    <a href="https://discord.gg/5eJwmkV"><img src="./assets/discord.svg" alt=""></a>
+    <a href="https://instagram.com/enigma.mu/"><img src="./assets/instagram.svg" alt=""></a>
+  </div>
+</template>

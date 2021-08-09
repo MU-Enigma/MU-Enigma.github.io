4b38e9e71fa62b5c6931c23050efb17e5eaf1900
Androbuddy <anirudhchimpidi8@gmail.com>
2021-08-09 23:40:42 +0530

    Setup header and footer
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="flex justify-center items-center px-8 py-5">
+    <ul class="flex lg:flex-row flex-col items-center gap-4 lg:gap-12">
+      <a href="mailto:engima@mahindrauniversity.edu.in" class="text-accent">
+        <li>
+          <div class="flex gap-2">
+            <span class="material-icons">mail</span>
+            <div>engima@mahindrauniversity.edu.in</div>
+          </div>
+        </li>
+      </a>
+      <li>Copyright © 2021 ENIGMA CS CLUB</li>
+      <li><Social /></li>
+    </ul>
+  </footer>
+</template>
+
+<script setup>
+import Social from './Social.vue'
+</script>
+
+<style>
+footer {
+  background: #111827;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="flex justify-between items-center px-8 py-5">
+    <img src="./assets/enigma.svg" class="lg:w-auto w-6" />
+    <ul class="lg:flex hidden gap-6">
+      <a href="" class="hover:text-accent transition ease-in-out duration-300 text-accent"><li>Home</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>About</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Blog</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Projects</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Events</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Gallery</li></a>
+    </ul>
+    <span class="material-icons lg:hidden" @click="overflow = true" v-if="!overflow">menu</span>
+    <span class="material-icons lg:hidden" @click="overflow = false" v-else>menu</span>
+  </header>
+  <div class="flex justify-center items-center absolute over-flow w-screen text-center" v-if="overflow">
+    <div class="">
+      <ul class="flex flex-col gap-12">
+      <a href="" class="hover:text-accent transition ease-in-out duration-300 text-accent"><li>Home</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>About</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Blog</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Projects</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Events</li></a>
+      <a href="" class="hover:text-accent transition ease-in-out duration-300"><li>Gallery</li></a>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      overflow: false
+    }
+  },
+}
+</script>
+
+<style>
+header {
+  background: #111827;
+}
+
+.over-flow {
+  background: rgba(0, 0, 0, 0.32);
+  margin-top: 64px;
+  height: calc(100vh - 64px);
+  -webkit-backdrop-filter: blur(8px);
+  backdrop-filter: blur(8px);
+}
+</style>

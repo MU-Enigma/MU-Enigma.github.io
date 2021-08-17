a438b0d2c844277bb0357d35d27d48207a8e159a
Androbuddy <anirudhchimpidi8@gmail.com>
2021-08-17 20:18:32 +0530

    Optimise images for gallery
    * TODO: implement a lazy loading method
    
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex gap-4">
-    <a href="https://github.com/mec-enigma"><img src="./assets/github.svg" alt=""></a>
+    <a href="https://github.com/mu-enigma"><img src="./assets/github.svg" alt=""></a>
     <a href="" class="hidden"><img src="./assets/linkedlin.svg" alt=""></a>
     <a href="https://discord.gg/5eJwmkV"><img src="./assets/discord.svg" alt=""></a>
     <a href="https://instagram.com/enigma.mu/"><img src="./assets/instagram.svg" alt=""></a>

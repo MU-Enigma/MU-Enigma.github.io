575c418f65e8b8ad22f1dafd938f129f0b9261ad
Androbuddy <anirudhchimpidi8@gmail.com>
2022-01-31 23:44:22 +0530

    LinkedIn letsgo
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-4">
     <a href="https://github.com/mu-enigma"><img src="./assets/github.svg" alt=""></a>
-    <a href="" class="hidden"><img src="./assets/linkedlin.svg" alt=""></a>
+    <a href="https://www.linkedin.com/company/mu-enigma/"><img src="./assets/linkedlin.svg" alt=""></a>
     <a href="https://www.youtube.com/channel/UCcq-LdW2k2_WpZHWq0i3TQQ"><img src="./assets/youtube.svg" alt=""></a>
     <a href="https://discord.gg/5eJwmkV"><img src="./assets/discord.svg" alt=""></a>
     <a href="https://instagram.com/enigma.mu/"><img src="./assets/instagram.svg" alt=""></a>

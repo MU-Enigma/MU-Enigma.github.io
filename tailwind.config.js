e512ada7fa1882b08eedd0e96dab66a7832c2996
Androbuddy <anirudhchimpidi8@gmail.com>
2022-03-08 22:37:44 +0530

    Updoot tailwind version
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],

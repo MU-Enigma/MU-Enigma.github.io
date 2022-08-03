3cce57a7747191cef4ad567960bb1175f6314af0
AndroBuddy <anirudhchimpidi8@gmail.com>
2022-08-03 13:00:54 +0530

    Move gallery assets to store
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-grow my-20 ml-10 lg:ml-44 mr-10 lg:mr-44">
-    <h1 class="text-accent font-bold text-3xl uppercase">Gallery</h1>
+    <h1 class="text-accent font-bold text-3xl">Gallery</h1>
     <div class="flex flex-col gap-14">
       <div class="flex flex-col gap-6 mt-4">
         <h2 class="text-2xl">Enigma hosted events</h2>
@@ -35,35 +35,38 @@ export default {
   data() {
       return {
         imgs: [
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P6A2461.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P6A2468.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P6A2473.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P6A2485.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P6A2509.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4377.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4453.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4493.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4720.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4722.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/Mozilla-Hackathon/3P7A4748.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_5769.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_5770.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_5780.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9876.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9885.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9887.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9906.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9916.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9943.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9946.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9953.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9955.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9963.JPG',
-          'https://github.com/MU-Enigma/MU-Enigma.github.io/raw/master/src/assets/images/All/IMG_9968.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P6A2461.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P6A2468.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P6A2473.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P6A2485.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P6A2509.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4377.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4453.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4493.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4720.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4722.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/Mozilla-Hackathon/3P7A4748.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_5769.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_5770.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_5780.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9876.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9885.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9887.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9906.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9916.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9943.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9946.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9953.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9955.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9963.JPG',
+          'https://raw.githubusercontent.com/MU-Enigma/store/master/gallery/All/IMG_9968.JPG',
         ],
         visible: false,
         index: 0
       }
+    },
+    created() {
+      window.scrollTo(0, 0)
     },
     methods: {
       showMultiple(index) {
